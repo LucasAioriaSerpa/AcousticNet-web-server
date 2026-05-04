57696a8ccca89b6add2dfb98387fc7c1e192d931
lucasAioriaSerpa <lucasaioriaserpa@outlook.com>
2026-05-04 12:21:22 -0500

    main.js

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,7 @@ fetch("/api/hello")
         `Status: ${response.status}`;
       throw new Error("HTTP error!");
     }
+    response.json();
   })
   .then((data) => {
     document.getElementById("resposta").textContent = JSON.stringify(
