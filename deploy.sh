ffdb961446394f1380e8fa3ae277b09c8a7659cc
Lucas Aioria Serpa <lucasaioriaserpa@outlook.com>
2026-06-14 13:22:10 -0300

    fix: colors in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,29 @@
 NC="\033[0m]"		#? No color
 GREEN="\033[0;32m"	#? Color Green
 
-echo "${GREEN}Parando & removendo containers antigos...${NC}"
+echo -e "${GREEN}Parando & removendo containers antigos...${NC}"
 docker rm -f frontend backend || true
 
-echo "${GREEN}Removendo volume antigo...${NC}"
+echo -e "${GREEN}Removendo volume antigo...${NC}"
 docker volume rm sqlite_data || true
 
-echo "${GREEN}Criando novo volume...${NC}"
+echo -e "${GREEN}Criando novo volume...${NC}"
 docker volume create sqlite_data
 
-echo "${GREEN}Reconstruindo a imagem do backend${NC}"
+echo -e "${GREEN}Reconstruindo a imagem do backend${NC}"
 docker build --no-cache -t acousticnet-backend ./backend
 
-echo "${GREEN}Criando e inicializando o container do backend${NC}"
+echo -e "${GREEN}Criando e inicializando o container do backend${NC}"
 docker run -d \
 	--name backend \
 	--network acousticnet \
 	-v sqlite_data:/app/data \
 	acousticnet-backend
 
-echo "${GREEN}Espera 3 segundos para que o backend seja inicializado${NC}"
+echo -e "${GREEN}Espera 3 segundos para que o backend seja inicializado${NC}"
 sleep 3
 
-echo "${GREEN}Criando e inicializando o container do frontend${NC}"
+echo -e "${GREEN}Criando e inicializando o container do frontend${NC}"
 docker run -d \
 	--name frontend \
 	--network acousticnet \
@@ -34,5 +34,5 @@ docker run -d \
 	-v ~/acousticnet/nginx.conf:/etc/nginx/conf.d/default.conf \
 	i386/nginx:alpine
 
-echo "${GREEN}Deploy realizado!${NC}"
+echo -e "${GREEN}Deploy realizado!${NC}"
 docker ps
